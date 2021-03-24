04f5669c8aadb68a45152eccaeba795c16252af3
raul <raul@raul.com>
2021-03-24 03:42:41 -0700

    checkpoint4

--- examen.py
+++ examen.py
@@ -1,8 +1,23 @@
 #from manage_cinema import Cinema
 
-def get_list(self, nombre_fichero):
+
+
+def get_list(nombre_fichero):
     nombre = nombre_fichero
     f = open (nombre, "rt", encoding="utf-8")
+    vacio == false
+
+    
 
 
-    return 0
+    linea = f.readline()
+
+
+    if linea == "":
+        raise ValueError("Linea")
+    else:
+        print ("lleno")
+    
+
+
+f1 = get_list("palabras.txt")
